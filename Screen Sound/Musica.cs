daa94cbc11e1ef62758b2b4138b792f92d33c139
RandomUser.js <arthurbarrosramos7@gmail.com>
2024-10-09 10:52:23 -0300

    Adicionar arquivos de projeto.

---
+++ Screen Sound/Musica.cs
@@ -0,0 +1,28 @@
+﻿class Musica
+{
+	public string Nome { get; set; }
+
+	public string Artista { get; set; }
+
+	public int Duracao { get; set; }
+
+	public bool Disponivel { get; set; }
+
+	public string DescricaoResumida =>
+		$"A música {Nome} pertence à banda {Artista}";
+
+	public void ExibirFichaTecnica()
+	{
+		Console.WriteLine($"Nome: {Nome}");
+		Console.WriteLine($"Artista: {Artista}");
+		Console.WriteLine($"Duração: {Duracao}s");
+		if (Disponivel)
+		{
+			Console.WriteLine("A música está dísponivel no seu plano");
+		}
+		else
+		{
+			Console.WriteLine("Esta música não está dísponivel no seu plano. Adquira o plano Plus+ agora!");
+		}
+	}
+}
